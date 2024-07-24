df96d452b4c0132c152c1c170c148c188ed5e8ad
lokyseo <lokyseo0434@nate.com>
2024-07-24 18:06:08 +0900

    에이스타 어지럽네

--- Assets/Scripts/A/Node.cs
+++ Assets/Scripts/A/Node.cs
@@ -6,10 +6,21 @@ public class Node
 {
     public bool walkable;
     public Vector3 worldPosition;
+    public int gridX;
+    public int gridY;
 
-    public Node(bool _walable, Vector3 _worldPos)
+    public int gCost;
+    public int hCost;
+    public Node parent;
+
+    public Node(bool _walable, Vector3 _worldPos, int _gridX, int _gridY)
     {
         walkable = _walable;
         worldPosition = _worldPos;
+        gridX = _gridX;
+        gridY = _gridY;
     }
+
+    public int fCost { get { return gCost + hCost; } }
+
 }
